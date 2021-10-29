c59305edda2ea8c7c5f7d4b154d33883efd9c59b
SENOL ISCI <senol.isci@gmail.com>
2021-10-29 13:16:59 +0100

    src changed

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            KUBERNETES on GCP. Please Scroll down!
+            SERVED by GOOGLE KUBERNETES ENGINE . Please Scroll down!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
